862b2d0a6a4d1bfa10ed939b135d270482693fbf
JPS4321 <104738904+JPS4321@users.noreply.github.com>
2023-07-19 20:46:34 -0600

    Update Driver.kt

--- Driver.kt
+++ Driver.kt
@@ -1,1 +1,6 @@
+// Esta parte del ejercicio fue hecha en el playground
+// Juan Pablo Solis
+// 22102
+// Programacion Plataformas Mobiles
+//
 class Driver(var name: String, var team: String, var country: String, var number: Int)
